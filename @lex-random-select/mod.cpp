5d5cee5a769058faeb3088c7db31c0ed0741f031
Jason Gassel <jason@gassel.io>
2018-02-23 21:45:12 -0600

    Release 0.1.0

--- @lex-random-select/mod.cpp
+++ @lex-random-select/mod.cpp
@@ -1,6 +1,6 @@
 name = "Random Select (ACE3 Action)";
 author = "lexnil";
-version = "1.0.0";
+version = "0.1.0";
 overview = "Select players at random for tasks/roles using ACE3 self interact.";
 actionName = "GitHub";
 action = "https://github.com/jpeg/arma3-random-select";
